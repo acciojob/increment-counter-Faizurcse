84ab9a56ba40e9253378a6a0dc795d525a2ead31
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-13 13:52:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
 let btn = document.getElementById('incrementBtn');
- let counter = 0;
+let counter = 0;
+
 btn.addEventListener('click', () => {
     let tags = document.getElementById('counter');
-    tags.innerText = counter = counter+1
+    counter = counter + 1;
+    tags.innerText = counter.toString(); // Convert counter to string before assigning
     alert(counter);
 });
